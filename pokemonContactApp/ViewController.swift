fbf32ef1bc2ea98a7fe96473144396bd61abd03e
zzaeun <leeje57575@gmail.com>
2025-09-26 12:17:19 +0900

    commit with .gitignore applied

---
+++ pokemonContactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokemonContactApp
+//
+//  Created by 이정은 on 9/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
