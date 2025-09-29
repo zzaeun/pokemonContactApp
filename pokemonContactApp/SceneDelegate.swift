8cbf2e6409754fbe3a95d68ae723dd727ec132f3
zzaeun <leeje57575@gmail.com>
2025-09-29 15:53:48 +0900

    [Chore] Lv2. "추가" 버튼 누르면 연락처 페이지로 넘어가게
    버튼, UINavigationController 기능 추가

--- pokemonContactApp/SceneDelegate.swift
+++ pokemonContactApp/SceneDelegate.swift
@@ -16,7 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
          let window = UIWindow(windowScene: windowScene)
 
-         window.rootViewController = ViewController()
+        let navigationVC = UINavigationController(rootViewController: ViewController())
+         window.rootViewController = navigationVC
          window.makeKeyAndVisible()
 
          self.window = window
